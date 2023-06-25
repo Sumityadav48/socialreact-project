c5c6a05a08817c9a9ff73a326b458f635a24e198
Sumityadav48 <sumitahir438@gmail.com>
2023-06-25 18:13:52 +0530

    setup gh-pages

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,7 +5,7 @@ export default function Topbar() {
   return (
     <div className='topbarContainer'>
         <div className="topbarLeft">
-          <span className="logo">Sumitsocial</span>
+          <span className="logo">SOCIALCLUB</span>
         </div>
         <div className="topbarCenter">
           <div className="searchbar">
